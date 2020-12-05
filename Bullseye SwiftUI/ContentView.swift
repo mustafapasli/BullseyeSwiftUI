bc8c2bd887d94120bdd007f4f6cfadf9500e0b8f
Mustafa Paslı <mustafapasli94@gmai.com>
2020-12-05 20:58:34 +0300

    Initial Commit

---
+++ Bullseye SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye SwiftUI
+//
+//  Created by Mustafa Paslı on 5.12.2020.
+//  Copyright © 2020 Mustafa Paslı. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
